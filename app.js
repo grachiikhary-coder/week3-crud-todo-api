ba943cc414872b459d4ed7233a76b64cb4bb6fc3
Ikhariale Grace <grachiikhary@gmail.com>
2026-05-26 18:32:28 +0100

    Add GET / todos/active endpoint and fix GET /todos/:id route

--- app.js
+++ app.js
@@ -14,9 +14,20 @@ app.get('/todos', (req, res) => {
 
 // POST New – Create
 app.post('/todos', (req, res) => {
-  const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
-  todos.push(newTodo);
-  res.status(201).json(newTodo); // Echo back
+  const {task, completed } = req.body; //
+
+  if (!task) {
+    return res.status(400).json({error: 'Task field is required'});
+  }
+
+  const newTodo = {
+    id: todos.length + 1,
+    task,
+    completed: completed || false
+  };
+
+  todos.push(newTodo); //
+    res.status(201).json(newTodo);
 });
 
 // PATCH Update – Partial
@@ -26,6 +37,22 @@ app.patch('/todos/:id', (req, res) => {
   Object.assign(todo, req.body); // Merge: e.g., {completed: true}
   res.status(200).json(todo);
 });
+
+//GET /todos/:id -Get one todo by id
+app.get ('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find(t => t.id === id);
+
+
+  if (!todo) {
+    return res.status(404).json({error: 'Todo not found'});
+  }
+
+  res.json(todo);
+}); //
+
+//GET Active - Get all todos where 
+
 
 // DELETE Remove
 app.delete('/todos/:id', (req, res) => {
